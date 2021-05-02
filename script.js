259604182cf8cc51ab53c26222815269e91bcfe6
Ryankawahara <ryryk1@gmail.com>
2021-05-02 13:30:14 -0400

    v2

--- script.js
+++ script.js
@@ -8,7 +8,34 @@ let searchBar;
 
 let imageArray = ["image01.png","image02.png","image03.png"];
 
+const cursor = document.querySelector('.cursor');
+
+AOS.init();
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+
+
+
+
 window.addEventListener('DOMContentLoaded', function(){
+
+console.log("loaded");
+
+
+
+
+
   movieBox = document.getElementById("movieListForSort");
   sortPreInternet = document.getElementById("preInternet");
   resetList = document.getElementById("resetButton");
@@ -35,17 +62,68 @@ window.addEventListener('DOMContentLoaded', function(){
     console.log(i+","+movieDatabase[i]["Title"])
     console.log(i+","+movieDatabase[i]["splideName"])
 
-    let splideName="#splideFor1984";
-    new Splide( "#image-slider2", {
+    new Splide( "#slideShow1984", {
       'cover'      : true,
-      'heightRatio': 0.5,
+      'next'  : 'splide__arrow--next arrowClass',
+
+
+      "width":"20vw",
+      "heightRatio":1.5,
+
+  //   '  autoWidth': true,
+	// 'focus'    : 'center',
+
+    } ).mount();
+
+    new Splide( "#slideShowFacialRecognition", {
+      'cover'      : true,
+      'next'  : 'splide__arrow--next arrowClass',
+
+
+      "width":"20vw",
+      "heightRatio":1.5,
+
+  //   '  autoWidth': true,
+  // 'focus'    : 'center',
+
+    } ).mount();
+
+    new Splide( "#slideShowCaptainAmerica", {
+      'cover'      : true,
+      "width":"20vw",
+      "heightRatio":1.5,
+    } ).mount();
+
+
+    new Splide( "#slideShowStrangeDays", {
+      'cover'      : true,
+      "width":"20vw",
+      "heightRatio":1.5,
+    } ).mount();
+
+    new Splide( "#slideShowEnemy", {
+      'cover'      : true,
+      "width":"20vw",
+      "heightRatio":1.5,
+    } ).mount();
+
+    new Splide( "#slideShowNerve", {
+      'cover'      : true,
+      "width":"20vw",
+      "heightRatio":1.5,
+    } ).mount();
+
+    new Splide( "#slideShowTrumanShow", {
+      'cover'      : true,
+      "width":"20vw",
+      "heightRatio":1.5,
     } ).mount();
 
   }
   // movieBox.innerText="";
 
   let options = {
-    valueNames: ['Title','Synopsis']
+    valueNames: ['Title','Synopsis','ReleaseYear','TakesPlaceIn']
   };
 
   let values = movieDatabase;
@@ -54,7 +132,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 
 
-  console.log(showThisJSON["FocusCardName"]);
+  // console.log(showThisJSON["FocusCardName"]);
 
   let slideShowSection = document.createElement("DIV");
   slideShowSection.id= "image-slider2";
@@ -97,10 +175,10 @@ window.addEventListener('DOMContentLoaded', function(){
   slideShowSection.appendChild(slideShowHolder);
   // moviePosterContainer.appendChild(slideShowSection);
   // console.log(slideShowSection);
-let appendToThis=document.getElementById("appendHere");
-let slideShow2 = document.getElementById("image-slider2");
-
-appendToThis.appendChild(slideShow2);
+// let appendToThis=document.getElementById("appendHere");
+// let slideShow2 = document.getElementById("slideShow1984");
+//
+// appendToThis.appendChild(slideShow2);
 })
 
 
@@ -108,54 +186,127 @@ let movieDatabase = [
 {
   "Title" : "1984",
   "splideName":"#splideFor1984",
-  "Synopsis":"In a totalitarian future society, a man, whose daily work is re-writing history, tries to rebel by falling in love. Longing for freedom, the lowly bureaucrat of the Ministry of Truth, Winston Smith, summons up the courage to write down his unspoken desires in his little secret diary.",
-  "FocusCardName":"1984card"
+  "Synopsis":"A man loses his identity while living under a repressive regime. In a story based on George Orwell's classic novel, Winston Smith (John Hurt) is a government employee whose job involves the rewriting of history in a manner that casts his fictional country's leaders in a charitable light. His trysts with Julia (Suzanna Hamilton) provide his only measure of enjoyment, but lawmakers frown on the relationship -- and in this closely monitored society, there is no escape from Big Brother.",
+  "ReleaseYear":"1984 (Based on George Orwell's novel from 1949)",
+  "TakesPlaceIn":"1984",
+  "FocusCardName":"card1984",
+  "AppendSign":"Append1984",
+  "beforeClickName":"beforeClickContent1984",
+  "SlideShowName":"slideShow1984"
 },
 
 {
   "Title" : "Captain America:\nThe Winter Soldier",
   "splideName":"#splideForCaptainAmerica",
   "Synopsis":"As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier.",
-  "FocusCardName":"CaptainAmericaCard"
+  "ReleaseYear":"2014",
+  "TakesPlaceIn":"2014",
+  "FocusCardName":"CaptainAmericaCard",
+  "beforeClickName":"beforeClickContentCaptainAmerica",
+  "AppendSign":"AppendCaptainAmerica",
+  "SlideShowName":"slideShowCaptainAmerica"
 
 
 },
 
 {
-  "Title":"Enemy of the State",
+  "Title" : "Strange Days",
+  "splideName":"#splideForStrangeDays",
+  "Synopsis":"Former policeman Lenny Nero (Ralph Fiennes) has moved into a more lucrative trade: the illegal sale of virtual reality-like recordings that allow users to experience the emotions and past experiences of others. While the bootlegs typically contain tawdry incidents, Nero is shocked when he receives one showing a murder. He enlists a friend, bodyguard Mace (Angela Bassett), to help find the killer -- and the two soon stumble upon a vast conspiracy involving the police force Nero once worked for.",
+  "ReleaseYear":"1995",
+  "TakesPlaceIn":"1999",
+  "FocusCardName":"StrangeDaysCard",
+  "beforeClickName":"beforeClickContentStrangeDaysCard",
+  "AppendSign":"AppendStrangeDays",
+  "SlideShowName":"slideShowStrangeDays"
+
+},
+
+{
+  "Title" : "Enemy of the State",
   "splideName":"#splideForEnemy",
-  "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
+  "Synopsis":"Corrupt National Security Agency official Thomas Reynolds (Jon Voight) has a congressman assassinated to assure the passage of expansive new surveillance legislation. When a videotape of the murder ends up in the hands of Robert Clayton Dean (Will Smith), a labor lawyer and dedicated family man, he is framed for murder. With the help of ex-intelligence agent Edward \"Brill\" Lyle (Gene Hackman), Dean attempts to throw Reynolds off his trail and prove his innocence.",
+  "ReleaseYear":"1998",
+  "TakesPlaceIn":"1998",
+  "FocusCardName":"EnemyCard",
+  "beforeClickName":"beforeClickContentEnemyCard",
+  "AppendSign":"AppendEnemy",
+  "SlideShowName":"slideShowEnemy"
+
+},
+
+{
+  "Title" : "Nerve",
+  "splideName":"#splideForNerve",
+  "Synopsis":"Industrious high school senior Vee Delmonico (Emma Roberts) is tired of living life on the sidelines. Pressured by her friends, Vee decides to join Nerve, a popular online game that challenges players to accept a series of dares. It's not long before the adrenaline-fueled competition requires her to perform increasingly dangerous stunts. When Nerve begins to take a sinister turn, Vee finds herself in a high-stakes finale that will ultimately determine her entire future.",
+  "ReleaseYear":"2016 (Based on Jeanne Ryan's novel from 2012)",
+  "TakesPlaceIn":"2016",
+  "FocusCardName":"NerveCard",
+  "beforeClickName":"beforeClickContentNerveCard",
+  "AppendSign":"AppendNerve",
+  "SlideShowName":"slideShowNerve"
 
 },
 {
-  "Title":"Enemy of the State",
-  "splideName":"#splideForEnemy",
-
-  "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
-
-},
-{
-  "Title":"Enemy of the State",
-  "splideName":"#splideForEnemy",
-
-  "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
+  "Title" : "The Truman Show",
+  "splideName":"#splideForTrumanShow",
+  "Synopsis":"He doesn't know it, but everything in Truman Burbank's (Jim Carrey) life is part of a massive TV set. Executive producer Christof (Ed Harris) orchestrates \"The Truman Show,\" a live broadcast of Truman's every move captured by hidden cameras. Cristof tries to control Truman's mind, even removing his true love, Sylvia (Natascha McElhone), from the show and replacing her with Meryl (Laura Linney). As Truman gradually discovers the truth, however, he must decide whether to act on it.",
+  "ReleaseYear":"1998",
+  "TakesPlaceIn":"2029",
+  "FocusCardName":"TrumanShowCard",
+  "beforeClickName":"beforeClickContentTrumanShowCard",
+  "AppendSign":"AppendTruman",
+  "SlideShowName":"slideShowTrumanShow"
 
 }
+
+
+//
+// {
+//   "Title":"Enemy of the State",
+//   "splideName":"#splideForEnemy",
+//   "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
+//
+// },
+// {
+//   "Title":"Enemy of the State",
+//   "splideName":"#splideForEnemy",
+//
+//   "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
+//
+// },
+// {
+//   "Title":"Enemy of the State",
+//   "splideName":"#splideForEnemy",
+//
+//   "Synopsis":"A lawyer becomes targeted by a corrupt politician and his N.S.A. goons when he accidentally receives key evidence to a politically motivated crime."
+//
+// }
 
 ]
 
 
 function displayJSON(showThisJSON){
 
+
 let movieList = document.createElement("LI");
+movieList.classList.add("listStyle");
+// movieList.classList.add("animatedParent");
 
   let movieFocus = document.createElement("DIV");
     movieFocus.classList.add('movieFocusStyle');
+    // movieFocus.classList.add('animated');
+    // movieFocus.classList.add('rollIn');
+    // movieFocus.classList.add('box');
+
+
+
 
     // movieFocus.classList.add(organizeThisJSON['specificStyleSheet']);
     // movieFocus.innerText=showThisJSON["Title"];
 
     let movieTitleSection = document.createElement("DIV");
+    // movieTitleSection.setAttribute("data-aos","zoom-out-up");
     movieTitleSection.classList.add('movieTitleSectionStyle');
     movieFocus.appendChild(movieTitleSection);
 
@@ -168,8 +319,25 @@ let movieList = document.createElement("LI");
     movieTitleSection.appendChild(movieTitleText);
 
 
+    let releaseYearSection = document.createElement("DIV");
+    releaseYearSection.classList.add('releaseYearSectionStyle');
+    releaseYearSection.innerText = "Released in " +showThisJSON["ReleaseYear"];
+    releaseYearSection.classList.add('ReleaseYear');
+
+    movieTitleSection.appendChild(releaseYearSection);
+
+
+        let predictedYearSection = document.createElement("DIV");
+        predictedYearSection.classList.add('predictedYearSectionStyle');
+      predictedYearSection.innerText = "Set in " +showThisJSON["TakesPlaceIn"];
+      predictedYearSection.classList.add('TakesPlaceIn');
+
+        movieTitleSection.appendChild(predictedYearSection);
+
 
     let movieSynopsisSection = document.createElement("DIV");
+
+
     movieSynopsisSection.classList.add('movieSynopsisSectionStyle');
     movieSynopsisSection.innerText = showThisJSON["Synopsis"];
     movieSynopsisSection.classList.add('Synopsis');
@@ -182,10 +350,15 @@ let movieList = document.createElement("LI");
     let twoPartSection = document.createElement("DIV");
     twoPartSection.classList.add('twoPartSectionStyle');
     movieFocus.appendChild(twoPartSection);
+      // movieFocus.setAttribute("data-aos","zoom-in-up");
+
 
      moviePosterContainer = document.createElement("DIV");
     moviePosterContainer.classList.add('moviePosterContainerStyle');
+    moviePosterContainer.id=showThisJSON["AppendSign"];
+
     twoPartSection.appendChild(moviePosterContainer);
+
 
     let movieTextSection = document.createElement("DIV");
     movieTextSection.classList.add('movieTextSectionStyle');
@@ -198,8 +371,11 @@ let movieList = document.createElement("LI");
     cardBody.classList.add('card-body');
 
 
-    let preClickText = document.createElement("p");
-    preClickText.innerText="testing";
+    // let preClickText = document.createElement("p");
+    let preClickText = document.getElementById(showThisJSON["beforeClickName"]);
+
+
+    // preClickText.innerText="testing";
 
     let linkText = document.createElement("p");
 
@@ -211,10 +387,14 @@ let movieList = document.createElement("LI");
     linkToWhere.setAttribute("href", "javascript:;");
 linkToWhere.innerText= "click";
 
-let displayWindowContents = document.createElement("DIV");
+// let displayWindowContents = document.createElement("DIV");
+ let displayWindowContents = document.getElementById(showThisJSON["FocusCardName"]);
+
 
 displayWindowContents.id= showThisJSON["FocusCardName"];
 displayWindowContents.classList.add("windowContentStyles");
+displayWindowContents.setAttribute("data-selectable","true");
+
 // displayWindowContents.id = "windowContentStyles2";
 
 
@@ -228,9 +408,9 @@ displayWindowContents.appendChild(windowHeaderText);
 
 
 linkText.appendChild(linkToWhere);
-cardBody.appendChild(linkText);
     cardBody.appendChild(preClickText);
     cardBody.appendChild(displayWindowContents);
+    cardBody.appendChild(linkText);
 
     focusCard.appendChild(cardBody);
     movieTextSection.appendChild(focusCard);
@@ -238,13 +418,25 @@ cardBody.appendChild(linkText);
 
 
 
-
-    movieList.appendChild(movieFocus);
+        let animateContainer = document.createElement("DIV");
+animateContainer.appendChild(movieFocus);
+animateContainer.classList.add("animatedParent");
+    movieList.appendChild(animateContainer);
+//     let animateContainer = document.createElement("DIV");
+// animateContainer.appendChild(movieFocus);
+// movielist.appendChiild(animateContainer);
     movieBox.appendChild(movieList);
 
 
 let cardName =showThisJSON["FocusCardName"];
+
 createSlideshow(cardName);
+
+
+let appendToThis=document.getElementById(showThisJSON["AppendSign"]);
+let slideShow2 = document.getElementById(showThisJSON["SlideShowName"]);
+
+appendToThis.appendChild(slideShow2);
 
 }
 
@@ -275,7 +467,7 @@ function createSlideshow(cardName){
 
   console.log(cardName);
 
-  moviePosterContainer.innerText=cardName;
+  // moviePosterContainer.innerText=cardName;
 
 let slideShowSection = document.createElement("DIV");
 slideShowSection.id= "image-slider";
